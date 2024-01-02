d5c14e9c8530b9659b8e5b00cf09da1708dae48f
letihab <letihabib1822@gmail.com>
2024-01-02 11:22:10 +0100

    add pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -3,5 +3,6 @@
 takes a float n as argument and returns
 the string representation of the float."""
 
+
 def to_str(n: float) -> str:
     return str(n)
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""type-annotated function sum_list which takes a list input_list of floats as argument
+"""type-annotated function sum_list
+which takes a list input_list of floats as argument
 and returns their sum as a float"""
 from typing import List
 
