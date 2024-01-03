7f2884c67ba6f9f0b269f2bf844f303677aa124c
letihab <letihabib1822@gmail.com>
2024-01-03 23:35:29 +0100

    check while requirement doesn't pass by adding newline after the cmment and before the import

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""Write an asynchronous coroutine"""
+"""
+Module to provide an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. This module spawns wait_random n times with
+the specified max_delay.
+"""
+
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -7,5 +12,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous coroutine that waits for a random delay and returns it."""
+
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+
     return sorted(delays)
