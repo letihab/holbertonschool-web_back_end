ca2221b0415eadd6ce4f15849db93f788fe8534a
letihab <letihabib1822@gmail.com>
2024-01-12 12:40:01 +0100

    add task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,10 +4,16 @@ function that takes two integer arguments and return
 a tuple size two containing a star index and end index
 """
 
+import typing
 
-def index_range(page, page_size):
+
+def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
+    """ return a tuple of size two containing a start
+    index and an end index corresponding to
+    the range of indexes to return in a
+    list for those particular pagination parametes"""
+
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
+    end_index = start_index + page_size
 
     return start_index, end_index
-
