1975538820194662a363f2af241bebf277ef49f5
letihab <letihabib1822@gmail.com>
2024-01-12 13:15:37 +0100

    update last task

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -4,12 +4,11 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-from typing import List, Dict
+from typing import Dict, List
 
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -18,8 +17,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -30,8 +28,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """
-        Dataset indexed by sorting position, starting at 0
+        """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -42,26 +39,31 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """
-        return a dictionary with the following key-value pairs
-        """
+        """Return a dictionary with pagination information."""
+        assert isinstance(index, int) and index >= 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        start_index = index * page_size if index is not None else 0
-        end_index = start_index + page_size - 1
+        dataset = self.indexed_dataset()
+        total_items = len(dataset)
 
-        assert start_index >= 0
-        assert end_index < len(self.dataset())
+        start = index
+        end = start + page_size
 
-        current_page = [self.dataset()[i] for i in range(start_index,
-                                                         min(end_index + 1,
-                                                             len(self.dataset(
-                                                             ))))]
+        # Validate that the start index is in a valid range
+        assert start <= total_items, f"Invalid start index: {start}"
 
-        next_index = end_index + 1
+        # If the end index is beyond the dataset, adjust it
+        end = min(end, total_items)
+
+        # Get the data for the current page
+        data = [dataset[i] for i in range(start, end)]
+
+        # Calculate the next index to query
+        next_index = end
 
         return {
-            'index': start_index,
-            'data': current_page,
-            'page_size': page_size,
-            'next_index': next_index
+            'index': start,
+            'next_index': next_index,
+            'page_size': len(data),
+            'data': data
         }
