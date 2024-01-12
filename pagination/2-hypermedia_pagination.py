213fe871e2a7ca825bb0280d19c9b4b3e433234c
letihab <letihabib1822@gmail.com>
2024-01-12 13:08:45 +0100

    update task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -36,8 +36,12 @@ class Server:
             assert isinstance(page_size, int) and page_size > 0
 
             start, end = index_range(page, page_size)
+            dataset = self.dataset()
 
-            return self.dataset()[start:end]
+            if start >= len(dataset):
+                return []
+
+            return dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size:
                   int = 10) -> dict[int, List[List]]:
