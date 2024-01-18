971641e68b9374f5d61a1326a732a046a32eef93
letihab <letihabib1822@gmail.com>
2024-01-18 14:29:58 +0100

    update task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,35 @@
+export default class Car {
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    get brand() {
+      return this._brand;
+    }
+  
+    set brand(newBrand) {
+      this._brand = newBrand;
+    }
+  
+    get motor() {
+      return this._motor;
+    }
+  
+    set motor(newMotor) {
+      this._motor = newMotor;
+    }
+  
+    get color() {
+      return this._color;
+    }
+  
+    set color(newColor) {
+      this._color = newColor;
+    }
+  
+    cloneCar() {
+      return new this.constructor();
+    }
+}
