d94b4ffe365a15d73513ad8bbf94f496c055c691
letihab <letihabib1822@gmail.com>
2024-01-18 13:44:47 +0100

    update task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,13 @@
 export default class Building {
     constructor(sqft) {
-      if (this.constructor !== Building && typeof this.evacuationWarningMessage === 'undefined') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-      this._sqft = sqft;
+        this._sqft = sqft;
+
+        if (this.constructor !== Building && typeof this.evacuationWarningMessage === 'undefined') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
     }
-  
+
     get sqft() {
-      return this._sqft;
-    }
-  
-    set sqft(newSqft) {
-      this._sqft = newSqft;
+        return this._sqft;
     }
 }
