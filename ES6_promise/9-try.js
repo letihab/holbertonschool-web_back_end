c67d16e10ab1d8c12d8c898484736cc8ee45a4d0
letihab <letihabib1822@gmail.com>
2024-01-26 15:33:04 +0100

    add task 8

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      const result = mathFunction();
-      queue.push(result);
-    } catch (error) {
-      queue.push('Error: cannot divide by 0');
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-  
-    return queue;
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push('Error: cannot divide by 0');
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
 }
