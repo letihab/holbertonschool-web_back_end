a7a35b924a254f922cbc82cf5ae67c3d85922d90
letihab <letihabib1822@gmail.com>
2024-02-04 20:01:04 +0100

    update task 7

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,4 @@
-// 7-set_has_values.js
-
 export default function hasValuesFromArray(set, array) {
-  // Use the every method to check if all elements in the array exist in the set
   return array.every((element) => set.has(element));
 }
  
